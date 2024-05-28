5ed67dc81e85c1f8e1ab0f30e356946f49ffff84
kishanjumnani <kishanimpjumnani@gmail.com>
2024-05-28 15:02:29 +0530

    day2classwrk

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Classwrk from './day2classwrk'
+// import './index.css'
+let a=23;
+ let b=25;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <Classwrk/>
+  </React.StrictMode>
+    
+   
+  
+)
